f9a872d31cd32668acf6240f6960da2ab2cc8a09
Warlocks1507Programmer <60792102+Warlocks1507Programmer@users.noreply.github.com>
2026-01-17 14:45:21 -0500

    Fixed Minion Motor

--- src/main/java/frc/robot/subsystems/MotorTest.java
+++ src/main/java/frc/robot/subsystems/MotorTest.java
@@ -1,57 +1,55 @@
 package frc.robot.subsystems;
 
-// WPILib
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static edu.wpi.first.units.Units.*;
 
-// CTRE Phoenix 6
-import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFXS;
-import com.ctre.phoenix6.signals.MotorArrangementValue;
-import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 
 import static frc.robot.Constants.MotorTest.*;
 
 public class MotorTest extends SubsystemBase {
 
-  //private final CANBus canbus = new CANBus("canivore");
-  private final TalonFXS motor = new TalonFXS(MOTOR_PORT);
-  private final VelocityVoltage m_velocityVoltage = new VelocityVoltage(0).withSlot(0);
+    private final TalonFXS motor = new TalonFXS(MOTOR_PORT);
+    private final VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
 
-  public MotorTest() {
-    // Create default hardware config
-    TalonFXSConfiguration config = new TalonFXSConfiguration();
+    public MotorTest() {
 
-    //config = new TalonFXSConfiguration();
+        TalonFXSConfiguration config = new TalonFXSConfiguration();
 
-    //config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
-    config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
+        // --- REQUIRED FOR MINION MODE ---
+        config.Commutation.MotorArrangement = MotorArrangementValue.Minion_JST;
 
-    motor.getConfigurator().apply(config);
+        // --- PID + FEEDFORWARD GAINS ---
+        config.Slot0.kS = 0.1;
+        config.Slot0.kV = 0.12;
+        config.Slot0.kP = 0.11;
+        config.Slot0.kI = 0.0;
+        config.Slot0.kD = 0.0;
 
-    // Configure PID/FF gains
-    config.Slot0.kS = 0.1;
-    config.Slot0.kV = 0.12;
-    config.Slot0.kP = 0.11;
-    config.Slot0.kI = 0;
-    config.Slot0.kD = 0;
+        // --- VOLTAGE LIMITS ---
+        config.Voltage.withPeakForwardVoltage(Volts.of(8))
+                      .withPeakReverseVoltage(Volts.of(-8));
 
-    // Voltage limits
-    config.Voltage.withPeakForwardVoltage(Volts.of(8))
-                  .withPeakReverseVoltage(Volts.of(-8));
+        // Apply configuration
+        motor.getConfigurator().apply(config);
+    }
 
-    // Apply config to the motor controller
-    motor.getConfigurator().apply(config);
-  }
+    /** Run motor in open-loop (percent output) */
+    public void runMotor(double power) {
+        motor.set(power);
+    }
 
-  public void runMotor(double power) {
-    motor.set(power);
-  }
+    /** Stop motor */
+    public void stopMotor() {
+        motor.set(0);
+    }
 
-  public void stopMotor() {
-    motor.set(0);
-  }
+    /** Run motor in closed-loop velocity mode (RPM) */
+    public void setRPM(double rpm) {
+        double rps = rpm / 60.0;
+        motor.setControl(velocityRequest.withVelocity(rps));
+    }
 }
